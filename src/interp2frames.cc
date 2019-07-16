5406493ebb0268d8ddb4160816ce5a142c24369c
Hubert Andre <hubert.andre.1991@gmail.com>
2019-07-16 16:35:47 +0200

    ok 1->2 to do 2->1

--- src/interp2frames.cc
+++ src/interp2frames.cc
@@ -18,21 +18,52 @@ static void drawOptFlowMap(const cv::Mat& flow, cv::Mat& cflowmap, int step,
     }
 }
 
+static cv::Mat ComputeInterpFrame(const cv::Mat& frame1, const cv::Mat& frame2,
+                                  const cv::Mat& flow, const double& t) {
+  cv::Mat iframe_12 = frame1 / 2 + frame2 / 2;
+  cv::Mat iframe(frame1.rows, frame1.cols, CV_8UC3);
+  cv::Mat count(frame1.rows, frame1.cols, CV_8U);
+  for (size_t y = 0; y < frame1.rows; y++) {
+    for (size_t x = 0; x < frame1.cols; x++) {
+      const cv::Point2f& fxy = flow.at<cv::Point2f>(y, x);
+      cv::Vec3b color1 = frame1.at<cv::Vec3b>(y, x);
+      cv::Vec3b color2 = frame2.at<cv::Vec3b>(int(y + fxy.y), int(x + fxy.x));
+      if (int(y + fxy.y * t) < frame1.rows &&
+          int(x + fxy.x * t) < frame1.cols && int(y + fxy.y * t) >= 0 &&
+          int(x + fxy.x * t) >= 0) {
+        count.at<char>(int(y + fxy.y * t), int(x + fxy.x * t)) =
+            1;  // TODO: check in paper if its normal that no double source
+
+        iframe.at<cv::Vec3b>(int(y + fxy.y * t), int(x + fxy.x * t)) =
+            color1 * (1. - t) + color2 * t;
+      }
+    }
+  }
+  for (size_t y = 0; y < frame1.rows; y++) {
+    for (size_t x = 0; x < frame1.cols; x++) {
+      if (count.at<char>(y, x) == 0) {
+        iframe.at<cv::Vec3b>(y, x) = frame1.at<cv::Vec3b>(y, x) * (1 - t) +
+                                     frame2.at<cv::Vec3b>(y, x) * t;
+      }
+    }
+  }
+  return iframe;
+}
 int main(int argc, char** argv) {
-  cv::Mat flow, cflow, frame, iframe;
+  cv::Mat flow, cflow, frame1, frame2;
   cv::UMat gray, prevgray, uflow;
   cv::namedWindow("flow", 1);
-
-  frame = cv::imread("/home/adlink/Pictures/1a.png");
-  cvtColor(frame, prevgray, cv::COLOR_BGR2GRAY);
-  frame = cv::imread("/home/adlink/Pictures/1b.png");
-  cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+  frame1 = cv::imread("/home/home/Pictures/1a.png");
+  cvtColor(frame1, prevgray, cv::COLOR_BGR2GRAY);
+  frame2 = cv::imread("/home/home/Pictures/1b.png");
+  cvtColor(frame2, gray, cv::COLOR_BGR2GRAY);
   calcOpticalFlowFarneback(prevgray, gray, uflow, 0.5, 3, 15, 3, 5, 1.2, 0);
   cvtColor(prevgray, cflow, cv::COLOR_GRAY2BGR);
-  uflow.copyTo(flow);
+  uflow.copyTo(flow);  // TODO: straight to flow
   drawOptFlowMap(flow, cflow, 16, 1.5, cv::Scalar(0, 255, 0));
   imshow("flow", cflow);
-
+  cv::Mat iframe = ComputeInterpFrame(frame1, frame2, flow, 0.5);
+  imshow("interpolated frame", iframe);
   cv::waitKey(0);
   return 0;
 }
